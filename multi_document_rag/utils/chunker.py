51183a31947bd9b79823355fbaf89333b9b59e24
muhammedfayiz122@gmail.com <muhammedfayiz122@gmail.com>
2025-07-25 18:21:40 +0530

    code updated

--- multi_document_rag/utils/chunker.py
+++ multi_document_rag/utils/chunker.py
@@ -1,8 +1,7 @@
 from langchain_experimental.text_splitter import SemanticChunker
-from multi_document_rag.utils.load_embeddings import load_embedding_model
 
-def doc_splitter(docs):
-    embeddings = load_embedding_model()
+
+def doc_splitter(embeddings, docs):
     text_splitter = SemanticChunker(embeddings=embeddings)
     splitted_doc = text_splitter.split_documents(docs)
     return splitted_doc
